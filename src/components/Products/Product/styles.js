13af37de90224a632af018197844eaa438576571
pupun-123 <65852883+pupun-123@users.noreply.github.com>
2022-06-23 10:33:25 +0530

    E-COMMERCE WEBSITE

---
+++ src/components/Products/Product/styles.js
@@ -0,0 +1,24 @@
+import { red } from '@material-ui/core/colors';
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  root: {
+    // maxWidth: 345, original width style
+    maxWidth: '100%',
+  },
+  color:{
+    backgroundColor:red
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  cardActions: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  cardContent: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+}));
